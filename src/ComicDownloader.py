93d67c8af00f51c714f295874a4579f04458f488
Le0-dot <lkolyadich@gmail.com>
2024-10-17 01:50:53 +0200

    Refarctor to use curl_cffi instead of requests. Use session to avoid bot detection. Use async versions of functions to improve performance.

--- src/ComicDownloader.py
+++ src/ComicDownloader.py
@@ -1,14 +1,13 @@
 import re
 import io
-import requests
+import asyncio
+from pathlib import Path
+from zipfile import ZipFile
+from argparse import ArgumentParser
+from dataclasses import dataclass, field
+from curl_cffi.requests import AsyncSession
 from selectolax.parser import HTMLParser, Node
 from PIL import Image
-from zipfile import ZipFile
-from dataclasses import dataclass
-from argparse import ArgumentParser
-from pathlib import Path
-from time import sleep
-from typing import Iterable
 
 
 @dataclass
@@ -16,9 +15,10 @@ class Img:
     filename: str
     url: str
 
-    def download_jpeg(self) -> bytes:
-        r = requests.get(self.url, stream=True)
-        image = Image.open(io.BytesIO(r.content))
+    async def download_jpeg(self, session: AsyncSession) -> bytes:
+        response = await session.get(self.url, stream=True)
+        data = await response.acontent()
+        image = Image.open(io.BytesIO(data))
         buffer = io.BytesIO()
         image.save(buffer, format="jpeg")
         return buffer.getvalue()
@@ -27,16 +27,18 @@ class Img:
 @dataclass
 class Archive:
     filename: str
-    imgs: Iterable[Img]
+    imgs: list[Img]
+    _imgs_data: list[bytes] = field(init=False)
+
+    async def download(self, session: AsyncSession) -> None:
+        tasks = [img.download_jpeg(session) for img in self.imgs]
+        self._imgs_data = await asyncio.gather(*tasks)
 
     def save(self, directory: Path):
         with ZipFile(directory / self.filename, "w") as archive:
-            for img in self.imgs:
-                sleep(0.5) # Throttle
-                if img.url:
-                    archive.writestr(img.filename, img.download_jpeg())
-                else:
-                    print(f"WARNING: no url ({img.url!r}) for {img.filename} in {directory / self.filename}")
+            for img, data in zip(self.imgs, self._imgs_data):
+                archive.writestr(img.filename, data)
+
 
 @dataclass
 class ImageConfig:
@@ -51,22 +53,27 @@ def img_from_node(node: Node, config: ImageConfig) -> Img | None:
     url = node.attributes[config.url_attr]
 
     if not url or not number:
-        print(f"Url or number for image is not present: url = {url!r}, number = {number!r}")
+        print(
+            f"Url or number for image is not present: url = {url!r}, number = {number!r}"
+        )
         return None
     if (match := re.search(config.number_pattern, number)) is None:
         print(f"No number found for image {url!r} in {number!r}")
         return None
 
-    return Img(match.group(0), url.strip())
+    return Img(f"{match.group(1):0>3}.jpg", url.strip())
 
 
-def find_images(url: str, config: ImageConfig) -> Iterable[Img]:
-    nodes = HTMLParser(requests.get(url).text).css(config.selector)
+async def find_images(
+    url: str, config: ImageConfig, session: AsyncSession
+) -> list[Img]:
+    response = await session.get(url)
+    nodes = HTMLParser(response.text).css(config.selector)
     images = (img_from_node(node, config) for node in nodes)
-    return (image for image in images if image is not None)
+    return [image for image in images if image is not None]
 
 
-def sequential_search(data: str, regexs: list[str]) -> str | None:
+def sequential_search(data: str, regexs: list[re.Pattern]) -> str | None:
     for regex in regexs:
         if (match := re.search(regex, data)) is None:
             return None
@@ -74,7 +81,7 @@ def sequential_search(data: str, regexs: list[str]) -> str | None:
     return data
 
 
-def get_name(url: str, regexs: list[str], padding: int | None) -> str:
+def get_name(url: str, regexs: list[re.Pattern], padding: int | None) -> str:
     if (name := sequential_search(url, regexs)) is None:
         raise ValueError(f"Regular Expression for name did not match for {url!r}")
     if padding:
@@ -88,13 +95,13 @@ def main():
         description="Given an URLs to a website with images, download and create cbz file from them.",
     )
 
-    parser.add_argument("URLs", nargs='+', help="urls with images to download")
+    parser.add_argument("URLs", nargs="+", help="urls with images to download")
     parser.add_argument(
         "-u",
         "--url-name-pattern",
         nargs="?",
         default=r"^.+/([^/]+)/?$",
-        help="a python regular expression with 1 capture group, that will be used to get filename for archive (default: filename in url)"
+        help="a python regular expression with 1 capture group, that will be used to get filename for archive (default: filename in url)",
     )
     parser.add_argument(
         "-d",
@@ -110,13 +117,13 @@ def main():
         nargs="?",
         const=3,
         type=int,
-        help="treat filename extracted with --url-name as a number and pad it to specified length (default length: 3)"
+        help="treat filename extracted with --url-name as a number and pad it to specified length (default length: 3)",
     )
     parser.add_argument(
         "-n",
         "--number",
         action="store_true",
-        help="find a number in name extracted by --url-name and use it"
+        help="find a number in name extracted by --url-name and use it",
     )
     parser.add_argument(
         "--image-selector",
@@ -146,9 +153,9 @@ def main():
     if not args.directory.is_dir():
         parser.exit(status=1, message=f"{args.directory} is not a directory")
 
-    regexs = [args.url_name_pattern]
+    regexs = [re.compile(args.url_name_pattern)]
     if args.number:
-        regexs.append(r"(\d+)")
+        regexs.append(re.compile(r"(\d+)"))
 
     config = ImageConfig(
         selector=args.image_selector,
@@ -157,9 +164,16 @@ def main():
         number_pattern=args.number_pattern,
     )
 
-    for url in args.URLs:
-        archive = Archive(get_name(url, regexs, args.padding), find_images(url, config))
-        archive.save(args.directory)
+    async def actions():
+        async with AsyncSession(impersonate="chrome") as session:
+            for url in args.URLs:
+                print(f"Processing {url!r}")
+                images = await find_images(url, config, session)
+                archive = Archive(get_name(url, regexs, args.padding), images)
+                await archive.download(session)
+                archive.save(args.directory)
+
+    asyncio.run(actions())
 
 
 if __name__ == "__main__":
